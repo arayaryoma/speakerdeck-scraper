c8877086ebc1eb00821cdab629c7bcb12f5c9be6
arayaryoma <r1o.ryoma.abe@gmail.com>
2020-12-30 01:15:52 +0900

    Implement parsing html and returning data

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,51 @@
 import { parse } from "node-html-parser";
-
 import fetch from "node-fetch";
-import { promisify } from "util";
 
 export interface FetchDecksOpts {
   username: string;
 }
 export interface Deck {
-  title: string;
-  published: Date;
+  id?: string;
+  title?: string;
+  url?: string;
+  previewImageSrc?: string;
 }
+
 export async function fetchDecks(opts: FetchDecksOpts): Promise<Array<Deck>> {
   const res = await fetch(`https://speakerdeck.com/${opts.username}`, {
     method: "GET",
   });
   const html = await res.text();
   const root = parse(html);
-  const decks = root.querySelector(
-    "div.container:nth-child(2) > div:nth-child(1) div"
-  );
 
-  debugger;
+  const decksEl = root
+    .querySelectorAll("div.container")[3]
+    .querySelector("div");
 
-  return [];
+  return decksEl
+    .querySelectorAll(".col-12.col-md-6.col-lg-4.mb-5")
+    .map((deckEl) => {
+      const anchor = deckEl.querySelector("a");
+      const preview = deckEl.querySelector(".card.deck-preview");
+
+      const deckId = preview.getAttribute("data-id");
+      const deckHref = anchor.getAttribute("href");
+      const deckUrl = deckHref
+        ? "https://speakerdeck.com" + deckHref
+        : undefined;
+      const title = anchor.getAttribute("title");
+      const previewImageSrc = (() => {
+        const rawUrl = preview.getAttribute("data-cover-image");
+        if (rawUrl === undefined) return undefined;
+        const url = new URL(rawUrl);
+        return url.origin + url.pathname;
+      })();
+
+      return {
+        id: deckId,
+        title,
+        previewImageSrc,
+        url: deckUrl,
+      };
+    });
 }
-
-const timeout = promisify(setTimeout);
-(async () => {
-  await fetchDecks({ username: "arayaryoma" });
-  await timeout(100000);
-})();
